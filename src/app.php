c077710cb571b5f8e102907e4e57213848f022e8
Justin Vencel <justin@citizennet.com>
2012-09-01 18:44:16 -0700

    initial commit

---
+++ src/app.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+
+$app = new Silex\Application();
+
+/**  Application */
+
+$app->register(new SessionServiceProvider());
+$app->register(new TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+
+/** Controller */
+$app->get('/', function() use($app) {
+	return $app['twig']->render('main.twig');
+});
+
+return $app;
---
+++ web/index.php
@@ -0,0 +1,6 @@
+<?php
+$app = require_once __DIR__.'/../src/app.php';
+
+$app['debug'] = true;
+
+$app->run();
